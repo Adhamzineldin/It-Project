8665f0a53b463f28e8f6eb8f889c0b409d7db345
Adham Mohamed Sabry Zineldin <mohalya3@gmail.com>
2024-04-24 00:50:05 +0200

    Added Mammals

--- js/app.js
+++ js/app.js
@@ -14,16 +14,13 @@ function scrollToSection(event) {
   const targetId = event.target.getAttribute('href').substring(1);
   const targetElement = document.getElementById(targetId);
   const headerOffset = document.querySelector('.Adham-Zineldin-header').offsetHeight;
-
   // Calculate target position
   const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - headerOffset;
-
-
   window.scrollTo({top: targetPosition, behavior: 'smooth'});
 
 }
 
-document.querySelectorAll('a[href^="#"]').forEach(link => {
+document.querySelectorAll('.Adham-Zineldin-nav, .Adham-Zineldin-navigation a[href^="#"]').forEach(link => {
   link.addEventListener('click', scrollToSection);
 });
 
@@ -57,7 +54,7 @@ if (loginForm) {
 
     if (username === 'test' && password === 'test') {
       elements.openModalBtn.style.display = 'none';
-      elements.userAvatar.src = 'img/avatar.jpg';
+      elements.userAvatar.src = '../resources/img/avatar.jpg';
       elements.userAvatar.style.display = 'block';
       elements.modal.style.display = 'none';
     } else {
@@ -72,3 +69,15 @@ document.querySelectorAll('.Adham-Zineldin-read-more-button').forEach(section =>
     window.location.href = this.getAttribute('data-link');
   });
 })
+
+
+
+//Adham Mostafa
+function togglePhoto() {
+    var photo = document.getElementById("AMSphoto");
+    if (photo.style.display === "none") {
+      photo.style.display = "block";
+    } else {
+      photo.style.display = "none";
+    }
+  }
