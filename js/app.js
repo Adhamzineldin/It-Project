ef99f44dcdc850aed5089b3e406ecb825ab117b5
Ayman-Elfeky <167029914+Ayman-Elfeky@users.noreply.github.com>
2024-04-25 17:55:24 +0200

    Merge branch 'main' of https://github.com/Adhamzineldin/It-Project

--- js/app.js
+++ js/app.js
@@ -1,20 +1,28 @@
 // Main Page (Adham Zineldin)
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-
-    });
-  });
-});
+function goBack() {
+  window.history.back();
+}
 
 document.querySelectorAll('.Adham-Zineldin-animal-section').forEach(section => {
   section.addEventListener('click', function () {
     window.location.href = this.getAttribute('data-link');
   });
 })
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const targetId = event.target.getAttribute('href').substring(1);
+  const targetElement = document.getElementById(targetId);
+  const headerOffset = document.querySelector('.Adham-Zineldin-header').offsetHeight;
+  // Calculate target position
+  const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - headerOffset;
+  window.scrollTo({top: targetPosition, behavior: 'smooth'});
+
+}
+
+document.querySelectorAll('.Adham-Zineldin-nav, .Adham-Zineldin-navigation a[href^="#"]').forEach(link => {
+  link.addEventListener('click', scrollToSection);
+});
 
 var elements = {
   modal: document.getElementById('loginModal'),
@@ -23,7 +31,7 @@ var elements = {
   userAvatar: document.getElementById('userAvatar'),
 };
 
-// Check if the required elements exist before using them
+
 if (elements.openModalBtn && elements.modal) {
   elements.openModalBtn.onclick = function () {
     elements.modal.style.display = 'block';
@@ -36,7 +44,7 @@ if (elements.closeBtn && elements.modal) {
   };
 }
 
-// Handle form submission
+
 var loginForm = document.getElementById('loginForm');
 if (loginForm) {
   loginForm.addEventListener('submit', function (event) {
@@ -46,7 +54,7 @@ if (loginForm) {
 
     if (username === 'test' && password === 'test') {
       elements.openModalBtn.style.display = 'none';
-      elements.userAvatar.src = 'img/avatar.jpg';
+      elements.userAvatar.src = '../resources/img/avatar.jpg';
       elements.userAvatar.style.display = 'block';
       elements.modal.style.display = 'none';
     } else {
@@ -61,3 +69,15 @@ document.querySelectorAll('.Adham-Zineldin-read-more-button').forEach(section =>
     window.location.href = this.getAttribute('data-link');
   });
 })
+
+
+
+//Adham Mostafa
+function togglePhoto() {
+    var photo = document.getElementById("AMSphoto");
+    if (photo.style.display === "none") {
+      photo.style.display = "block";
+    } else {
+      photo.style.display = "none";
+    }
+  }
