231aeb5524abe48355c40fb3d1ca3ec11b521df4
Adham Mohamed Sabry Zineldin <mohalya3@gmail.com>
2024-04-29 13:28:35 +0300

    localstorage

--- js/app.js
+++ js/app.js
@@ -24,44 +24,6 @@ document.querySelectorAll('.Adham-Zineldin-nav, .Adham-Zineldin-navigation a[hre
   link.addEventListener('click', scrollToSection);
 });
 
-var elements = {
-  modal: document.getElementById('loginModal'),
-  openModalBtn: document.getElementById('openModalBtn'),
-  closeBtn: document.getElementById('closeBtn'),
-  userAvatar: document.getElementById('userAvatar'),
-};
-
-
-if (elements.openModalBtn && elements.modal) {
-  elements.openModalBtn.onclick = function () {
-    elements.modal.style.display = 'block';
-  };
-}
-
-if (elements.closeBtn && elements.modal) {
-  elements.closeBtn.onclick = function () {
-    elements.modal.style.display = 'none';
-  };
-}
-
-
-var loginForm = document.getElementById('loginForm');
-if (loginForm) {
-  loginForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-
-    if (username === 'test' && password === 'test') {
-      elements.openModalBtn.style.display = 'none';
-      elements.userAvatar.src = '../resources/img/avatar.jpg';
-      elements.userAvatar.style.display = 'block';
-      elements.modal.style.display = 'none';
-    } else {
-      alert('Invalid username or password. Please try again.');
-    }
-  });
-}
 
 //Birds Adham Zineldin
 document.querySelectorAll('.Adham-Zineldin-read-more-button').forEach(section => {
@@ -71,13 +33,12 @@ document.querySelectorAll('.Adham-Zineldin-read-more-button').forEach(section =>
 })
 
 
-
 //Adham Mostafa
 function togglePhoto() {
-    var photo = document.getElementById("AMSphoto");
-    if (photo.style.display === "none") {
-      photo.style.display = "block";
-    } else {
-      photo.style.display = "none";
-    }
+  var photo = document.getElementById("AMSphoto");
+  if (photo.style.display === "none") {
+    photo.style.display = "block";
+  } else {
+    photo.style.display = "none";
   }
+}
